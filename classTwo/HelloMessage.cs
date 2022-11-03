a8db4ac039f348419dfed3d19badc24b113b258d
ricardo.paulino.nascimento <ricardo.paulino.nascimento@hotmail.com>
2022-11-02 22:59:14 -0300

    referencia externa

---
+++ classTwo/HelloMessage.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Windows.Forms;
+
+class HelloMessage{
+    public void Speak(){
+        MessageBox.Show("Hello...");
+    }
+}
---
+++ classTwo/Program.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Program{
+    static void Main(){
+        HelloMessage h = new HelloMessage();
+        h.Speak();
+    }
+}
