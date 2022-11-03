782c87b1ad8d62c794e89bc20596f29f0ffada8f
ricardo.paulino.nascimento <ricardo.paulino.nascimento@hotmail.com>
2022-11-02 22:45:33 -0300

    first commit

---
+++ classOne/Testing.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Windows.Forms;
+
+namespace classOne.Testing{
+    public class Testing{
+        static void Main(){
+            Console.WriteLine("Testing! 1, 2, 3");
+            MessageBox.Show("Testing interface");
+        }
+    }
+}
